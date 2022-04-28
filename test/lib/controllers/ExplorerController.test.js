57d2defa09f741f52e0f7afe771244ce627cfcd2
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-04-27 22:56:08 -0500

    Agregando correcciones de estilo con linter

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -3,39 +3,41 @@ const Reader = require("../../../lib/utils/Reader");
 const ExplorerController = require("../../../lib/controllers/ExplorerController");
 
 describe("Unit Test for Explorer Controller", () => {
-  test("Get explorers by mission", () => {
-    const explorers = Reader.readJsonFile("explorers.json");
-    const explorersFiltered = explorers.filter(
-      (explorer) => explorer.mission == "node"
-    );
-    const explorersByMission = ExplorerController.getExplorersByMission("node");
-    expect(explorersByMission).toStrictEqual(explorersFiltered);
-  });
-  test("Explorers usernames by mission", () => {
-    const explorers = Reader.readJsonFile("explorers.json");
-    const explorersInNodeToGetUsernames = explorers.filter(
-      (explorer) => explorer.mission == "node"
-    );
-    const usernamesInNode = explorersInNodeToGetUsernames.map(
-      (explorer) => explorer.githubUsername
-    );
-    const explorersUsernames = ExplorerController.getExplorersUsernamesByMission(
-      "node");
-    expect(explorersUsernames).toStrictEqual(usernamesInNode);
-  });
-  test("Get explorers amount by mission", () => {
-    const amontOfExplorersInMission = ExplorerController.getExplorersAmonutByMission(
-      "node");
-    expect(amontOfExplorersInMission).toBe(10);
-  });
-  test("Validate score", () => {
-    const scoreFB1 = ExplorerController.validateScore(1);
-    const scoreFB3 = ExplorerController.validateScore(3);
-    const scoreFB5 = ExplorerController.validateScore(5);
-    const scoreFB15 = ExplorerController.validateScore(15);
-    expect(scoreFB1).toBe(1);
-    expect(scoreFB3).toBe('FIZZ');
-    expect(scoreFB5).toBe('BUZZ');
-    expect(scoreFB15).toBe('FIZZBUZZ');
-  });
+    test("Get explorers by mission", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersFiltered = explorers.filter(
+            (explorer) => explorer.mission == "node",
+        );
+        const explorersByMission = ExplorerController.getExplorersByMission("node");
+        expect(explorersByMission).toStrictEqual(explorersFiltered);
+    });
+    test("Explorers usernames by mission", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersInNodeToGetUsernames = explorers.filter(
+            (explorer) => explorer.mission == "node",
+        );
+        const usernamesInNode = explorersInNodeToGetUsernames.map(
+            (explorer) => explorer.githubUsername,
+        );
+        const explorersUsernames = ExplorerController.getExplorersUsernamesByMission(
+            "node",
+        );
+        expect(explorersUsernames).toStrictEqual(usernamesInNode);
+    });
+    test("Get explorers amount by mission", () => {
+        const amontOfExplorersInMission = ExplorerController.getExplorersAmonutByMission(
+            "node",
+        );
+        expect(amontOfExplorersInMission).toBe(10);
+    });
+    test("Validate score", () => {
+        const scoreFB1 = ExplorerController.validateScore(1);
+        const scoreFB3 = ExplorerController.validateScore(3);
+        const scoreFB5 = ExplorerController.validateScore(5);
+        const scoreFB15 = ExplorerController.validateScore(15);
+        expect(scoreFB1).toBe(1);
+        expect(scoreFB3).toBe("FIZZ");
+        expect(scoreFB5).toBe("BUZZ");
+        expect(scoreFB15).toBe("FIZZBUZZ");
+    });
 });
