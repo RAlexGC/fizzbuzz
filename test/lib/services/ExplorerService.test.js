89fb4752983840fce498fbaeb1a313dddecda08f
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-04-27 19:50:41 -0500

    Agregando pruebas de unidad para Explorer Service

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -10,6 +10,13 @@ describe("Unit Test for Explorer Service", () => {
     })
     test("Amount of explorers by mission", () => {
         const amountOfExplorers = ExplorerService.getAmountOfExplorersByMission('explorers.json','node')
-        expect()
+        expect(amountOfExplorers).toBe(10)
+    })
+    test("Get explorers username by mission", () => {
+        const explorers = Reader.readJsonFile('explorers.json')
+        const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
+        const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
+        const explorersUsernames = ExplorerService.getExplorersUsernamesByMission('explorers.json','node')
+        expect(explorersUsernames).toStrictEqual(usernamesInNode)
     })
 })
